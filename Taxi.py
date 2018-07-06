e8edf9813ff7211ad854a3842dd395a267490cdc
Kirill <30757466+Kirili4ik@users.noreply.github.com>
2018-07-06 21:41:33 +0300

    v4

--- Taxi.py
+++ Taxi.py
@@ -1,15 +1,15 @@
 import numpy as np
 import gym
 import copy
-
+import matplotlib.pyplot as plt
 
 class Agent:
 
     def __init__(self, env, alpha, gamma):
         self.env = env
-        nA = env.action_space.n + 4         # +4 ?
+        nA = env.action_space.n + 4  # +4 ?
         nS = env.observation_space.n
-        self.V = np.zeros((nA, nS))        # V for primitive and C for others, dicts?
+        self.V = np.zeros((nA, nS))  # V for primitive and C for others ?
         self.C = np.zeros((nA, nS, nA))
         # s, n, e, w, pickup, dropoff, goto, put, get, root
         # 0, 1, 2, 3, 4,      5,       6,    7,   8,   9
@@ -27,18 +27,21 @@ class Agent:
         ]
         self.alpha = alpha
         self.gamma = gamma
-        self.taken = False
         self.r_sum = 0
         self.new_s = copy.copy(self.env.s)
-
+        self.done = False
 
     def is_terminal(self, a, done):
         RGBY = [(0, 0), (0, 4), (4, 0), (4, 3)]
         taxirow, taxicol, passidx, destidx = list(self.env.decode(self.env.s))  # env.s == state now
-        if a == 9 or a == 7:
+        if done:
+            return True
+        elif a == 9:
             return done
         elif a == 8:
             return passidx >= 4
+        elif a == 7:
+            return passidx < 4
         elif a == 6:
             return (passidx < 4 and (taxirow, taxicol) == RGBY[passidx] or
                     passidx >= 4 and (taxirow, taxicol) == RGBY[destidx])
@@ -46,7 +49,7 @@ class Agent:
             return True
 
     def evaluate(self, i, s):
-        if i <= 5:               # primitive action
+        if i <= 5:  # primitive action
             return self.V[i, s]
         else:
             for j in self.graph[i]:
@@ -60,25 +63,37 @@ class Agent:
     # e-Greedy Approach
     def greed_act(self, i, s):
         e = 0.1
-        Q = np.arange(0)                                                     # |
+        Q = np.arange(0)
+        for a2 in self.graph[i]:
+            Q = np.concatenate((Q, [self.V[a2, s] + self.C[i, s, a2]]))
+        max_arg = np.argmax(Q)
+        possible_a = np.array(list(self.graph[i]))
+        if np.random.rand(1) < e:
+            return np.random.choice(possible_a)
+        else:
+            return possible_a[max_arg]
+        '''Q = np.arange(0)                                                  # |
         for a2 in self.graph[i]:                                             # |  def count_Q ?
             Q = np.concatenate((Q, [self.V[a2, s] + self.C[i, s, a2]]))      # |
         max_arg = np.argmax(Q)                                               # |
         policy = np.zeros(len(Q)) + e / len(Q)
         policy[max_arg] += 1 - e
         possible_a = np.array(list(self.graph[i]))
-        return np.random.choice(possible_a, p=policy)  # choose from children with probabilities for explor/exploit prob
+        return np.random.choice(possible_a, p=policy)  # choose from children with probabilities for explor/exploit prob'''
 
     def MAXQ_0(self, i, s):
-        done = False
-        if i <= 5:                                          # primitive action
-            self.new_s, r, done, _ = copy.copy(self.env.step(i))
+        if self.done:
+            i = 10
+        self.done = False
+        if i <= 5:  # primitive action
+            self.new_s, r, self.done, _ = copy.copy(self.env.step(i))
             self.r_sum += r
             self.V[i, s] += self.alpha * (r - self.V[i, s])
+            # self.env.render()
             return 1
-        else:
+        elif i <= 9:
             count = 0
-            while not self.is_terminal(i, done):
+            while not self.is_terminal(i, self.done):
                 a = self.greed_act(i, s)
                 N = self.MAXQ_0(a, s)
                 # s' = new_s
@@ -90,17 +105,26 @@ class Agent:
 
     def reset(self, new_env):
         self.env = new_env
+        taxi.r_sum = 0
+        self.done = False
+        self.new_s = copy.copy(self.env.s)
 
-### MAIN PROGRAM
-#still infinite loop - greed_act probably doesn't work properly
 
+        
+###   MAIN PROGRAM
+# no infinite cycle, but not correct neither
 alpha = 0.1
 gamma = 0.999
 env = gym.make('Taxi-v2').env
 taxi = Agent(env, alpha, gamma)
-episodes = 100
+episodes = 500
+sum_list = []
 for j in range(episodes):
     env.reset()
     taxi.reset(env)
     taxi.MAXQ_0(9, env.s)
-    print(taxi.r_sum, end=" ")
+    sum_list.append(taxi.r_sum)
+print(sum_list)
+plt.plot(sum_list)
+plt.legend('rewards', 'episodes')
+plt.show()
