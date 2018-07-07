914bdbf1cf323ae3f28982bbf9d40a641149bd1b
Kirill <30757466+Kirili4ik@users.noreply.github.com>
2018-07-07 17:12:27 +0300

    Update Taxi.py
    Problems left + something for checking left

--- Taxi.py
+++ Taxi.py
@@ -2,7 +2,8 @@ import numpy as np
 import gym
 import copy
 import matplotlib.pyplot as plt
-import time
+import time     # for checks
+
 
 class Agent:
 
@@ -10,17 +11,17 @@ class Agent:
         self.env = env
         nA = env.action_space.n + 4
         nS = env.observation_space.n
-        self.V = np.zeros((nA, nS))     # V for primitive and C for others
+        self.V = np.zeros((nA, nS))  # V for primitive and C for others, make dicts?
         self.C = np.zeros((nA, nS, nA))
         # s, n, e, w, pickup, dropoff, goto, put, get, root
         # 0, 1, 2, 3, 4,      5,       6,    7,   8,   9
         self.graph = [
-            set(),  # 0 s
-            set(),  # 1 n
-            set(),  # 2 e
-            set(),  # 3 w
-            set(),  # 4 pickup
-            set(),  # 5 dropoff
+            set(),
+            set(),
+            set(),
+            set(),
+            set(),
+            set(),
             {0, 1, 2, 3},  # 6 goto -> s, n, e, w
             {5, 6},  # 7 put -> dropoff, goto
             {4, 6},  # 8 get -> pickup, goto
@@ -32,25 +33,31 @@ class Agent:
         self.new_s = copy.copy(self.env.s)
         self.done = False
 
+    def is_incar(self, passidx):
+        return passidx >= 4
+    
+    def is_primitive(self, i):
+        return i <= 5
+    
     def is_terminal(self, a, done):
-        RGBY = [(0, 0), (0, 4), (4, 0), (4, 3)]
+        RGBY = [(0, 0), (0, 4), (4, 0), (4, 3)]         # from env
         taxirow, taxicol, passidx, destidx = list(self.env.decode(self.env.s))
         if done:
             return True
-        elif a == 9:
+        elif a == 9:    # root
             return done
-        elif a == 8:
-            return passidx >= 4
-        elif a == 7:
-            return passidx < 4
-        elif a == 6:
-            return (passidx < 4 and (taxirow, taxicol) == RGBY[passidx] or
-                    passidx >= 4 and (taxirow, taxicol) == RGBY[destidx])
-        elif a <= 5:
+        elif a == 8:    # get
+            return self.is_incar(passidx)
+        elif a == 7:    # put
+            return not self.is_incar(passidx)
+        elif a == 6:    # goto
+            return (not self.is_incar(passidx) and (taxirow, taxicol) == RGBY[passidx] or
+                    self.is_incar(passidx) and (taxirow, taxicol) == RGBY[destidx])
+        elif self.is_primitive(a):
             return True
 
-    def evaluate(self, i, s):
-        if i <= 5:                  # primitive action
+    def evaluate(self, i, s):       # probably got problems
+        if self.is_primitive(i):
             return self.V[i, s]
         else:
             for j in self.graph[i]:
@@ -58,7 +65,7 @@ class Agent:
             Q = np.arange(0)
             for a2 in self.graph[i]:
                 Q = np.concatenate((Q, [self.V[a2, s] + self.C[i, s, a2]]))
-            max_arg = np.argmax(Q)
+            max_arg = np.argmax(Q)                                           
             return self.V[max_arg, s]
 
     # e-Greedy Approach
@@ -74,10 +81,11 @@ class Agent:
         else:
             return possible_a[max_arg]
         
-        '''Q = np.arange(0)
-        for a2 in self.graph[i]:
-            Q = np.concatenate((Q, [self.V[a2, s] + self.C[i, s, a2]]))
-        max_arg = np.argmax(Q)
+        # Another way
+        '''Q = np.arange(0)                                                  
+        for a2 in self.graph[i]:                                             
+            Q = np.concatenate((Q, [self.V[a2, s] + self.C[i, s, a2]]))      
+        max_arg = np.argmax(Q)                                              
         policy = np.zeros(len(Q)) + e / len(Q)
         policy[max_arg] += 1 - e
         possible_a = np.array(list(self.graph[i]))
@@ -87,16 +95,19 @@ class Agent:
         if self.done:
             i = 10
         self.done = False
-        if i <= 5:  # primitive action
+        if self.is_primitive(i):
             self.new_s, r, self.done, _ = copy.copy(self.env.step(i))
             self.r_sum += r
             self.V[i, s] += self.alpha * (r - self.V[i, s])
+            # self.env.render()
+            # time.sleep(0.7)
             return 1
-        elif i <= 9:
+        elif i <= 9:            # not primitive, but exists
             count = 0
             while not self.is_terminal(i, self.done):
                 a = self.greed_act(i, s)
                 N = self.MAXQ_0(a, s)
+                # s' = new_s
                 self.V[i, self.new_s] = self.evaluate(i, self.new_s)
                 self.C[i, s, a] += self.alpha * (self.gamma ** N * self.V[i, self.new_s] - self.C[i, s, a])
                 count += N
@@ -109,7 +120,8 @@ class Agent:
         self.done = False
         self.new_s = copy.copy(self.env.s)
 
-#####     MAIN PROGRAM
+### MAIN PROGRAM
+# at ~1100-1200 episodes ?
 
 alpha = 0.1
 gamma = 0.999
@@ -118,9 +130,12 @@ taxi = Agent(env, alpha, gamma)
 episodes = 2000
 sum_list = []
 for j in range(episodes):
+    # if j == 1170:
+        # print('go')
+        # abcdef = input()
     taxi.reset()
     taxi.MAXQ_0(9, env.s)
     sum_list.append(taxi.r_sum)
-print(sum_list)
+    # print(j)
 plt.plot(sum_list)
 plt.show()
