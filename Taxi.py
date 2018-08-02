ae98b238388a4d5f990822a16856d79c9080d209
Kirill <30757466+Kirili4ik@users.noreply.github.com>
2018-08-02 13:39:52 +0300

    First working properly version
    Changed evaluate, e-greedy
    Have to add checking for terminal actions in evaluate function

--- Taxi.py
+++ Taxi.py
@@ -2,114 +2,101 @@ import numpy as np
 import gym
 import copy
 import matplotlib.pyplot as plt
-import time     # for checks
+import time
 
 
 class Agent:
-
     def __init__(self, env, alpha, gamma):
         self.env = env
-        nA = env.action_space.n + 4
+        nA = env.action_space.n + 5           # + goto * 2 + put + get + root
         nS = env.observation_space.n
-        self.V = np.zeros((nA, nS))  # V for primitive and C for others, make dicts?
+        self.V = np.zeros((nA, nS))
         self.C = np.zeros((nA, nS, nA))
-        # s, n, e, w, pickup, dropoff, goto, put, get, root
-        # 0, 1, 2, 3, 4,      5,       6,    7,   8,   9
+        self.V_copy = self.V.copy()
         self.graph = [
-            set(),
-            set(),
-            set(),
-            set(),
-            set(),
-            set(),
-            {0, 1, 2, 3},  # 6 goto -> s, n, e, w
-            {5, 6},  # 7 put -> dropoff, goto
-            {4, 6},  # 8 get -> pickup, goto
-            {7, 8},  # 9 root -> put, get
+            set(),  # 0 s
+            set(),  # 1 n
+            set(),  # 2 e
+            set(),  # 3 w
+            set(),  # 4 pickup
+            set(),  # 5 dropoff
+            {0, 1, 2, 3},  # 6 gotoSource -> s, n, e, w
+            {0, 1, 2, 3},  # 7 gotoDestination -> s, n, e, w
+            {4, 6},  # 8 get -> pickup, gotoS
+            {5, 7},  # 9 put -> dropoff, gotoD
+            {8, 9},  # 10 root -> put, get
         ]
         self.alpha = alpha
         self.gamma = gamma
         self.r_sum = 0
         self.new_s = copy.copy(self.env.s)
         self.done = False
+        self.num_of_ac = 0
 
-    def is_incar(self, passidx):
-        return passidx >= 4
-    
-    def is_primitive(self, i):
-        return i <= 5
-    
     def is_terminal(self, a, done):
-        RGBY = [(0, 0), (0, 4), (4, 0), (4, 3)]         # from env
+        RGBY = [(0, 0), (0, 4), (4, 0), (4, 3)]
         taxirow, taxicol, passidx, destidx = list(self.env.decode(self.env.s))
+        taxiloc = (taxirow, taxicol)
         if done:
             return True
-        elif a == 9:    # root
+        elif a == 10:
             return done
-        elif a == 8:    # get
-            return self.is_incar(passidx)
-        elif a == 7:    # put
-            return not self.is_incar(passidx)
-        elif a == 6:    # goto
-            return (not self.is_incar(passidx) and (taxirow, taxicol) == RGBY[passidx] or
-                    self.is_incar(passidx) and (taxirow, taxicol) == RGBY[destidx])
-        elif self.is_primitive(a):
+        elif a == 9:
+            return passidx < 4
+        elif a == 8:
+            return passidx >= 4
+        elif a == 7:
+            return passidx >= 4 and taxiloc == RGBY[destidx]
+        elif a == 6:                                         # добавил наличие/отсутствие пассажира в условия
+            return passidx < 4 and taxiloc == RGBY[passidx]
+        elif a <= 5:
             return True
 
-    def evaluate(self, i, s):       # probably got problems
-        if self.is_primitive(i):
-            return self.V[i, s]
-        else:
-            for j in self.graph[i]:
-                self.V[j, s] = self.evaluate(j, s)
-            Q = np.arange(0)
-            for a2 in self.graph[i]:
-                Q = np.concatenate((Q, [self.V[a2, s] + self.C[i, s, a2]]))
-            max_arg = np.argmax(Q)                                           
-            return self.V[max_arg, s]
+    def evaluate(self, i, s):
+            if i <= 5:                          # primitive action
+                return self.V_copy[i, s]
+            else:
+                for j in self.graph[i]:
+                    self.V_copy[j, s] = self.evaluate(j, s)
+                Q = np.arange(0)
+                for a2 in self.graph[i]:
+                    Q = np.concatenate((Q, [self.V_copy[a2, s]]))
+                max_arg = np.argmax(Q)
+                return self.V_copy[max_arg, s]
 
     # e-Greedy Approach
     def greed_act(self, i, s):
-        e = 0.1
+        e = 0.001
         Q = np.arange(0)
+        possible_a = np.arange(0)
         for a2 in self.graph[i]:
-            Q = np.concatenate((Q, [self.V[a2, s] + self.C[i, s, a2]]))
+            if a2 <= 5 or (not self.is_terminal(a2, self.done)):
+                Q = np.concatenate((Q, [self.V[a2, s] + self.C[i, s, a2]]))
+                possible_a = np.concatenate((possible_a, [a2]))
         max_arg = np.argmax(Q)
-        possible_a = np.array(list(self.graph[i]))
         if np.random.rand(1) < e:
             return np.random.choice(possible_a)
         else:
             return possible_a[max_arg]
         
-        # Another way
-        '''Q = np.arange(0)                                                  
-        for a2 in self.graph[i]:                                             
-            Q = np.concatenate((Q, [self.V[a2, s] + self.C[i, s, a2]]))      
-        max_arg = np.argmax(Q)                                              
-        policy = np.zeros(len(Q)) + e / len(Q)
-        policy[max_arg] += 1 - e
-        possible_a = np.array(list(self.graph[i]))
-        return np.random.choice(possible_a, p=policy)'''
-
     def MAXQ_0(self, i, s):
         if self.done:
-            i = 10
+            i = 13                  # to end
         self.done = False
-        if self.is_primitive(i):
+        if i <= 5:                  # primitive action
             self.new_s, r, self.done, _ = copy.copy(self.env.step(i))
             self.r_sum += r
+            self.num_of_ac += 1
             self.V[i, s] += self.alpha * (r - self.V[i, s])
-            # self.env.render()
-            # time.sleep(0.7)
             return 1
-        elif i <= 9:            # not primitive, but exists
+        elif i <= 10:
             count = 0
             while not self.is_terminal(i, self.done):
                 a = self.greed_act(i, s)
                 N = self.MAXQ_0(a, s)
-                # s' = new_s
-                self.V[i, self.new_s] = self.evaluate(i, self.new_s)
-                self.C[i, s, a] += self.alpha * (self.gamma ** N * self.V[i, self.new_s] - self.C[i, s, a])
+                self.V_copy = self.V.copy()
+                evaluate_res = self.evaluate(i, self.new_s)
+                self.C[i, s, a] += self.alpha * (self.gamma ** N * evaluate_res - self.C[i, s, a])
                 count += N
                 s = self.new_s
             return count
@@ -117,25 +104,21 @@ class Agent:
     def reset(self):
         self.env.reset()
         self.r_sum = 0
+        self.num_of_ac = 0
         self.done = False
         self.new_s = copy.copy(self.env.s)
 
-### MAIN PROGRAM
-# at ~1100-1200 episodes ?
 
-alpha = 0.1
-gamma = 0.999
+alpha = 0.2
+gamma = 1
 env = gym.make('Taxi-v2').env
 taxi = Agent(env, alpha, gamma)
-episodes = 2000
+episodes = 10000
 sum_list = []
 for j in range(episodes):
-    # if j == 1170:
-        # print('go')
-        # abcdef = input()
     taxi.reset()
-    taxi.MAXQ_0(9, env.s)
+    taxi.MAXQ_0(10, env.s)
     sum_list.append(taxi.r_sum)
-    # print(j)
+    print(j)
 plt.plot(sum_list)
 plt.show()
