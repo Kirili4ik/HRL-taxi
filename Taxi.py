5617629a49e77a1a84232b895019edffdbe9df56
Kirill <30757466+Kirili4ik@users.noreply.github.com>
2019-11-03 14:23:23 +0300

    Rearranged, v2 -> v3
    Got rid of most constants, changed comments, blank lines, changed version to v3

--- Taxi.py
+++ Taxi.py
@@ -8,24 +8,40 @@ import time
 class Agent:
     def __init__(self, env, alpha, gamma):
         self.env = env
-        nA = env.action_space.n + 5           # + goto * 2 + put + get + root
+        
+        hier_act_sp = 2 + 1 + 1 + 1   # goto * 2 + put + get + root
+        nA = env.action_space.n + hier_act_sp       
         nS = env.observation_space.n
         self.V = np.zeros((nA, nS))
         self.C = np.zeros((nA, nS, nA))
         self.V_copy = self.V.copy()
+        
+        s = self.south = 0
+        n = self.north = 1
+        e = self.east = 2
+        w = self.west = 3
+        pickup = self.pickup = 4
+        dropoff = self.dropoff = 5
+        gotoS = self.gotoS = 6
+        gotoD = self.gotoD = 7
+        get = self.get = 8
+        put = self.put = 9
+        root = self.root = 10
+        
         self.graph = [
-            set(),  # 0 s
-            set(),  # 1 n
-            set(),  # 2 e
-            set(),  # 3 w
-            set(),  # 4 pickup
-            set(),  # 5 dropoff
-            {0, 1, 2, 3},  # 6 gotoSource -> s, n, e, w
-            {0, 1, 2, 3},  # 7 gotoDestination -> s, n, e, w
-            {4, 6},  # 8 get -> pickup, gotoS
-            {5, 7},  # 9 put -> dropoff, gotoD
-            {8, 9},  # 10 root -> put, get
+            set(),  # south
+            set(),  # north
+            set(),  # east
+            set(),  # west
+            set(),  # pickup
+            set(),  # dropoff
+            {s, n, e, w},  # gotoSource
+            {s, n, e, w},  # gotoDestination
+            {pickup, gotoS},  # get -> pickup, gotoSource
+            {dropoff, gotoD},  # put -> dropoff, gotoDestination
+            {put, get},  # root -> put, get
         ]
+        
         self.alpha = alpha
         self.gamma = gamma
         self.r_sum = 0
@@ -33,65 +49,77 @@ class Agent:
         self.done = False
         self.num_of_ac = 0
 
+        
+    def is_primitive(self, act):
+        if (act <= 5):
+            return True
+        else:
+            return False
+    
+    
     def is_terminal(self, a, done):
         RGBY = [(0, 0), (0, 4), (4, 0), (4, 3)]
         taxirow, taxicol, passidx, destidx = list(self.env.decode(self.env.s))
         taxiloc = (taxirow, taxicol)
         if done:
             return True
-        elif a == 10:
+        elif a == self.root:
             return done
-        elif a == 9:
+        elif a == self.put:
             return passidx < 4
-        elif a == 8:
+        elif a == self.get:
             return passidx >= 4
-        elif a == 7:
+        elif a == self.gotoD:
             return passidx >= 4 and taxiloc == RGBY[destidx]
-        elif a == 6:                                         # добавил наличие/отсутствие пассажира в условия
+        elif a == self.gotoS:
             return passidx < 4 and taxiloc == RGBY[passidx]
-        elif a <= 5:
+        elif self.is_primitive(a):
+            # just else
             return True
 
-    def evaluate(self, i, s):
-            if i <= 5:                          # primitive action
-                return self.V_copy[i, s]
+        
+    def evaluate(self, act, s):
+            if self.is_primitive(act):
+                return self.V_copy[act, s]
             else:
-                for j in self.graph[i]:
+                for j in self.graph[act]:
                     self.V_copy[j, s] = self.evaluate(j, s)
                 Q = np.arange(0)
-                for a2 in self.graph[i]:
+                for a2 in self.graph[act]:
                     Q = np.concatenate((Q, [self.V_copy[a2, s]]))
                 max_arg = np.argmax(Q)
                 return self.V_copy[max_arg, s]
 
-    # e-Greedy Approach
-    def greed_act(self, i, s):
+            
+    # e-Greedy Approach with eps=0.001
+    def greed_act(self, act, s):
         e = 0.001
         Q = np.arange(0)
         possible_a = np.arange(0)
-        for a2 in self.graph[i]:
-            if a2 <= 5 or (not self.is_terminal(a2, self.done)):
-                Q = np.concatenate((Q, [self.V[a2, s] + self.C[i, s, a2]]))
-                possible_a = np.concatenate((possible_a, [a2]))
+        for act2 in self.graph[act]:
+            if self.is_primitive(act2) or (not self.is_terminal(act2, self.done)):
+                Q = np.concatenate((Q, [self.V[act2, s] + self.C[act, s, act2]]))
+                possible_a = np.concatenate((possible_a, [act2]))
         max_arg = np.argmax(Q)
         if np.random.rand(1) < e:
             return np.random.choice(possible_a)
         else:
             return possible_a[max_arg]
         
-    def MAXQ_0(self, i, s):
+        
+    def MAXQ_0(self, i, s):  # i is action number
         if self.done:
-            i = 13                  # to end
+            i = 11                  # to end recursion
         self.done = False
-        if i <= 5:                  # primitive action
+        if self.is_primitive(i):
             self.new_s, r, self.done, _ = copy.copy(self.env.step(i))
             self.r_sum += r
             self.num_of_ac += 1
             self.V[i, s] += self.alpha * (r - self.V[i, s])
             return 1
-        elif i <= 10:
+        elif i <= self.root:
             count = 0
-            while not self.is_terminal(i, self.done):
+            while not self.is_terminal(i, self.done): # a is new action num
                 a = self.greed_act(i, s)
                 N = self.MAXQ_0(a, s)
                 self.V_copy = self.V.copy()
@@ -101,6 +129,7 @@ class Agent:
                 s = self.new_s
             return count
 
+        
     def reset(self):
         self.env.reset()
         self.r_sum = 0
@@ -111,14 +140,15 @@ class Agent:
 
 alpha = 0.2
 gamma = 1
-env = gym.make('Taxi-v2').env
+env = gym.make('Taxi-v3').env
 taxi = Agent(env, alpha, gamma)
-episodes = 10000
+episodes = 10001
 sum_list = []
 for j in range(episodes):
     taxi.reset()
-    taxi.MAXQ_0(10, env.s)
+    taxi.MAXQ_0(10, env.s)      # start in root
     sum_list.append(taxi.r_sum)
-    print(j)
+    if (j % 1000 == 0):
+        print('already made', j, 'episodes')
 plt.plot(sum_list)
 plt.show()
